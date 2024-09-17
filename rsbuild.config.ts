0ee2e311335174c7c2d323688c4ca22318f2bcd0
Douglas Pardim <douglas.pardim@globalsys.com.br>
2024-09-17 00:39:12 -0300

    fix: added app configs

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  html: {
+    template: './public/index.html',
+  },
+  security: {
+    nonce: 'CSP_NONCE_PLACEHOLDER',
+  },
   plugins: [pluginReact()],
-});
+})
